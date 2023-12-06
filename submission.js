dd045722c23416897edad7ab3f9d257f0724224c
Jose Padolina <jose.p.padolina@gmail.com>
2023-12-05 20:27:43 -0800

    Update submission.js

--- submission.js
+++ submission.js
@@ -1,15 +1,9 @@
 const findSum = function(array) {
-  if (!Array.isArray(array)) {
-    throw new Error('Input must be an array of numbers');
-  }
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    if (typeof array[i] !== 'number') {
-      throw new Error('Array must contain only numbers');
-    }
-    sum += array[i];
-  }
-  return sum;
+let total = 0;
+for( let num of array){
+    total += num;
+}
+return total;
 };
 
 const findFrequency = function(array) {
@@ -42,11 +36,8 @@ const findFrequency = function(array) {
 };
 
 const isPalindrome = function(str) {
-  if (typeof str !== 'string') {
-    throw new Error('Input must be a string');
-  }
 
-  for (let i = 0; i < Math.floor(str.length / 2); i++) {
+  for (let i = 0; i < str.length; i++) {
     if (str[i] !== str[str.length - 1 - i]) {
       return false;
     }
@@ -56,51 +47,32 @@ const isPalindrome = function(str) {
 };
 
 const largestPair = function(array) {
-  let largestNumber = array[0] * array[1];
+    array.sort((a, b) => b - a);
 
-  for (let i = 1; i < array.length - 1; i++) {
-    const currentNumber = array[i] * array[i + 1];
-    if (currentNumber > largestNumber) {
-      largestNumber = currentNumber;
-    }
-  }
-
-  return largestNumber;
+    return array[0] + array[1];
 
 };
 
 const removeParenth = function(str) {
-  if (typeof str !== 'string') {
-    throw new Error('');
-  }
-
-  let openParenth = -1;
-  let closeParenth = -1;
-
+ let count = 0;
+  let output = "";
   for (let i = 0; i < str.length; i++) {
-    if (str[i] === '(') {
-      openParenth = i;
-    } else if (str[i] === ')') {
-      closeParenth = i;
-      break;
+    if (str[i] === "(") {
+      count++;
+    } else if (str[i] === ")") {
+      count--;
+     
+    }
+    if (count === 0) {
+      output += str[i];
     }
   }
-
-  if (openParenth === -1 || closeParenth === -1) {
-    throw new Error('');
-  }
-
-  const stringWithoutParenth = str.slice(0, openParenth) + str.slice(closeParenth + 1);
-  return stringWithoutParenth;
+  return output;
 
 };
 
 const scoreScrabble = function(str) {
-  if (typeof str !== 'string') {
-    throw new Error('Input must be a string');
-  }
-
-  const letterPoints = {
+  const letterValues = {
     'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 'r': 1, 's': 1, 't': 1,
     'd': 2, 'g': 2,
     'b': 3, 'c': 3, 'm': 3, 'p': 3,
@@ -110,13 +82,9 @@ const scoreScrabble = function(str) {
     'q': 10, 'z': 10
   };
 
- 
   let score = 0;
-  for (let i = 0; i < str.length; i++) {
-    const currentLetter = str[i];
-    if (letterPoints.hasOwnProperty(currentLetter)) {
-      score += letterPoints[currentLetter];
-    }
+  for (const letter of str) {
+    score += letterValues[letter] || 0;
   }
 
   return score;
